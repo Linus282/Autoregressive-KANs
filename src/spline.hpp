79936a273bf0cd3d10427aa5af812edd02f688f3
Linus282 <linus.nudingg@gmail.com>
2025-07-18 17:23:08 +0200

    Update spline.hpp

--- src/spline.hpp
+++ src/spline.hpp
@@ -1,15 +1,10 @@
-#pragma once
-#include <vector>
-
 class Spline {
 public:
     Spline(const std::vector<double>& knots);
-
     double evaluate(double x) const;
-    void setWeights(const std::vector<double>& w);
+    void setCoefficients(const std::vector<double>& coeffs);  // ⬅️ hinzufügen
 
 private:
     std::vector<double> knots_;
-    std::vector<double> weights_;
-    int findInterval(double x) const;
+    std::vector<double> coeffs_;
 };
