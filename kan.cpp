523e6f8a924e2d666f86902c568825230d3cf8e1
Linus282 <linus.nudingg@gmail.com>
2025-07-18 16:26:23 +0200

    Create kan.cpp

---
+++ kan.cpp
@@ -0,0 +1,24 @@
+#include "kan.hpp"
+
+KAN::KAN(int input_dim, int hidden_units, const std::vector<double>& knots)
+    : input_dim_(input_dim), hidden_units_(hidden_units) {
+    for (int i = 0; i < hidden_units_; ++i)
+        splines_.emplace_back(knots);
+    linear_weights_ = std::vector<std::vector<double>>(hidden_units_, std::vector<double>(input_dim_, 1.0));
+}
+
+void KAN::setWeights(int neuron, const std::vector<double>& w, const std::vector<double>& coeffs) {
+    linear_weights_[neuron] = coeffs;
+    splines_[neuron].setWeights(w);
+}
+
+double KAN::forward(const std::vector<double>& x) {
+    double out = 0.0;
+    for (int i = 0; i < hidden_units_; ++i) {
+        double z = 0.0;
+        for (int j = 0; j < input_dim_; ++j)
+            z += linear_weights_[i][j] * x[j];
+        out += splines_[i].evaluate(z);
+    }
+    return out;
+}
