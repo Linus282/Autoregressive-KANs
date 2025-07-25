c9ac5cb9399154a11610c3b0a5cdaccc979a05c4
Linus282 <linus.nudingg@gmail.com>
2025-07-25 17:04:01 +0200

    Update pykan.cpp

--- bindings/pykan.cpp
+++ bindings/pykan.cpp
@@ -1,10 +1,16 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "../src/kan.hpp"  // Pfad zur Header-Datei anpassen
+#include "../src/kan.hpp"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(pykan, m) {
+    py::enum_<BasisType>(m, "BasisType")
+        .value("LinearSpline", BasisType::LinearSpline)
+        .value("Sin", BasisType::Sin)
+        .value("Tanh", BasisType::Tanh)
+        .export_values();
+
     py::class_<KAN>(m, "KAN")
         .def(py::init<int, int, const std::vector<double>&, BasisType>())
         .def("forward", &KAN::forward)
